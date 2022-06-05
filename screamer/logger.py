fa0827d71a94b7430ba6c4e6757421a34e928d8a
samicrusader <hi@samicrusader.me>
2022-06-04 20:25:42 -0400

    fix: logging names

--- screamer/logger.py
+++ screamer/logger.py
@@ -3,7 +3,7 @@ import logging
 
 # https://stackoverflow.com/a/7622029
 def setup_custom_logger(name):
-    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
