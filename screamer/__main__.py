264d7d8c480e7279bd1ce1abbb5256436f021be0
samicrusader <hi@samicrusader.me>
2022-06-04 22:18:09 -0400

    fix: tcp server doesnt fail every 15 seconds

--- screamer/__main__.py
+++ screamer/__main__.py
@@ -1,9 +1,11 @@
 # import argparse
 from . import logger
 from .hdhomerun import discover
+from _thread import start_new_thread
 import logging
 import socket
 import os
+import time
 import threading
 from flask import Flask
 
@@ -76,41 +78,61 @@ class CreateLLMNRServer:
             self.log.log(logging.INFO, f'Message from Client: {message}')
             self.log.log(logging.INFO, f'Client IP Address: {address}')
 
+
 class CreateTCPControlServer:
     log = logging.getLogger('tcpcontrol')
     log.setLevel(logging.DEBUG)
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-    def run(self, ip: str, port: int):
-        self.log.log(logging.INFO, f'Listening on {ip if ip else "*"}:{port}')
-        self.tcp_socket.bind((ip, port))
+    def handle(self, conn, addr):
         while True:
-            message, address = self.tcp_socket.recvfrom(1460)
+            try:
+                message = conn.recv(1460)
+                if not message:
+                    self.log.log(logging.INFO, f'Client {addr} dropped.')
+                    break
+                self.log.log(logging.DEBUG, f'Message from Client: {message}')
+                conn.send(b'testmsg')
+                conn.close()
+            except OSError as e:
+                self.log.log(logging.INFO, f'Client {addr} dropped.')
+                break
 
-            self.log.log(logging.INFO, f'Message from Client: {message}')
-            self.log.log(logging.INFO, f'Client IP Address: {address}')
+    def run(self, ip: str, port: int):
+        self.tcp_socket.bind((ip, port))
+        self.log.log(logging.INFO, f'Listening on {ip if ip else "*"}:{port}')
+        self.tcp_socket.listen()
+        while True:
+            connection, address = self.tcp_socket.accept()
+            self.log.log(logging.INFO, f'Client {address} connected.')
+            start_new_thread(self.handle, (connection, address))
+
 
 if __name__ == '__main__':
     webgui_thread = threading.Thread(
         target=lambda: create_http_mgmt_server().run(host='127.0.0.1', port=8080, use_reloader=False, threaded=True),
         daemon=True)
     webgui_thread.start()
+    time.sleep(1)
     http_stream_thread = threading.Thread(
         target=lambda: create_http_stream_server().run(host='127.0.0.1', port=5004, use_reloader=False, threaded=True),
         daemon=True)
     http_stream_thread.start()
+    time.sleep(1)
     broadcast_thread = threading.Thread(
         target=lambda: CreateUDPBroadcastServer().run(ip='', port=65001),
         daemon=True)
     broadcast_thread.start()
+    time.sleep(1)
     control_thread = threading.Thread(
         target=lambda: CreateTCPControlServer().run(ip='', port=65001),
         daemon=True)
     control_thread.start()
+    time.sleep(1)
     llmnr_thread = threading.Thread(
-        target=lambda: CreateLLMNRServer().run(ip='224.0.0.252', port=5355),
+        target=lambda: CreateLLMNRServer().run(ip='', port=5355),
         daemon=True)
-    llmnr_thread.start()
-    for thread in [webgui_thread, http_stream_thread, broadcast_thread]:
+    #llmnr_thread.start() # not needed for now
+    for thread in [webgui_thread, http_stream_thread, broadcast_thread, control_thread, llmnr_thread]:
         thread.join()
