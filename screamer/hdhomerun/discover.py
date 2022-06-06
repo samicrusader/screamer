cf6b4b7c3a80385d2155096580d80cc3d559a208
samicrusader <hi@samicrusader.me>
2022-06-06 06:09:35 -0400

    add: dummy data and hdhomerun connect quatro spoof

--- screamer/hdhomerun/discover.py
+++ screamer/hdhomerun/discover.py
@@ -3,7 +3,7 @@ from .packets import create
 def discover_request(payload: bytes):
     payload = 0x02.to_bytes(1, 'big')  # HDHOMERUN_TAG_DEVICE_ID
     payload += 0x04.to_bytes(1, 'big')
-    payload += b'\x10\x10\xc0\x32'  # device id
+    payload += b'\x10\x7c\x21\xc8'  # device id, https://github.com/Silicondust/libhdhomerun/blob/032728af66da1eff490e5b22d0427a314c93fa31/hdhomerun_discover.c ??
 
     payload += 0x01.to_bytes(1, 'big')  # HDHOMERUN_TAG_DEVICE_TYPE
     payload += 0x04.to_bytes(1, 'big')
