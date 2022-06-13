e4240eefdb877d29c0515d9d8f56a195507af065
samicrusader <hi@samicrusader.me>
2022-06-13 04:08:04 -0400

    refactor: change hdhr specific functions names

--- screamer/__main__.py
+++ screamer/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 from . import logger
-from .hdhomerun import control, discover
-from .hdhomerun.packets import parse
+from .hdhomerun import control as hdhrcontrol, discover as hdhrdiscover
+from .hdhomerun.packets import parse as hdhrparse
 from _thread import start_new_thread
 import logging
 import socket
@@ -14,8 +14,8 @@ log = logger.setup_custom_logger('root')
 config = dict()
 
 
-def create_http_mgmt_server():
-    flask = Flask('screamer_mgmt')
+def hdhr_create_http_mgmt_server():
+    flask = Flask('screamer_hdhr_mgmt')
     flask.app_config = config
 
     if not os.path.exists(flask.instance_path):
@@ -26,8 +26,8 @@ def create_http_mgmt_server():
     return flask
 
 
-def create_http_stream_server():
-    flask = Flask('screamer_http_stream')
+def hdhr_create_http_stream_server():
+    flask = Flask('screamer_hdhr_http_stream')
     flask.app_config = config
 
     if not os.path.exists(flask.instance_path):
@@ -38,9 +38,9 @@ def create_http_stream_server():
     return flask
 
 
-class CreateUDPBroadcastServer:
-    log = logging.getLogger('broadcast')
-    log.setLevel(logging.DEBUG)
+class HDHRCreateUDPBroadcastServer:
+    log = logging.getLogger('hdhr_broadcast')
+    log.setLevel(logging.INFO)
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -53,12 +53,12 @@ class CreateUDPBroadcastServer:
             self.log.log(logging.INFO, f'Client {address} connected.')
             self.log.log(logging.DEBUG, f'Message from Client: {message}')
 
-            x = parse(message)
+            x = hdhrparse(message)
             self.log.log(logging.DEBUG, f'Packet type: {x[0]}')
             self.log.log(logging.DEBUG, f'Packet payload: {x[1]}')
             match x[0]:
                 case 'discover_request':
-                    func = discover.discover_request
+                    func = hdhrdiscover.discover_request
                 case _:
                     self.log.log(logging.INFO, f'Client {address} sent invalid request.')
                     break
@@ -71,8 +71,8 @@ class CreateUDPBroadcastServer:
             # udp_server_socket.sendto(bytesToSend, address)
 
 
-class CreateLLMNRServer:
-    log = logging.getLogger('llmnr')
+class HDHRCreateLLMNRServer:
+    log = logging.getLogger('hdhr_llmnr')
     log.setLevel(logging.DEBUG)
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
@@ -87,9 +87,9 @@ class CreateLLMNRServer:
             self.log.log(logging.DEBUG, f'Message from Client: {message}')
 
 
-class CreateTCPControlServer:
-    log = logging.getLogger('tcpcontrol')
-    log.setLevel(logging.DEBUG)
+class HDHRCreateTCPControlServer:
+    log = logging.getLogger('hdhr_tcpcontrol')
+    log.setLevel(logging.INFO)
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
@@ -102,12 +102,12 @@ class CreateTCPControlServer:
                     break
 
                 self.log.log(logging.DEBUG, f'Message from Client: {message}')
-                x = parse(message)
+                x = hdhrparse(message)
                 self.log.log(logging.DEBUG, f'Packet type: {x[0]}')
                 self.log.log(logging.DEBUG, f'Packet payload: {x[1]}')
                 match x[0]:
                     case 'getset_request':
-                        func = control.getset
+                        func = hdhrcontrol.getset
                     case _:
                         self.log.log(logging.INFO, f'Client {address} sent invalid request.')
                         conn.close()
@@ -195,8 +195,10 @@ if __name__ == '__main__':
         exit(1)
 
     # Parse config variables
-    try: toml_entry = tomlconfig['server']
-    except KeyError: toml_entry = dict()
+    try:
+        toml_entry = tomlconfig['server']
+    except KeyError:
+        toml_entry = dict()
 
     for i in [
         ('server', ['bind', 'webgui_port', 'enable_webgui', 'enable_httpstream', 'enable_broadcast', 'enable_control']),
@@ -207,11 +209,13 @@ if __name__ == '__main__':
             if not getattr(args, x) == None:
                 cfg_entry[x] = getattr(args, x)
             else:
-                try: tomlconfig[i[0]][x]
+                try:
+                    tomlconfig[i[0]][x]
                 except KeyError:
                     log.log(logging.ERROR, f'Config variable {i[0]}[{x}] is not set. Bailing out.')
                     exit(1)
-                else: cfg_entry[x] = tomlconfig[i[0]][x]
+                else:
+                    cfg_entry[x] = tomlconfig[i[0]][x]
         config[i[0]] = cfg_entry
 
     config['channels'] = channeldata
@@ -220,37 +224,37 @@ if __name__ == '__main__':
     # Configure and start threads
     threads = list()
     if config['server']['enable_webgui'] == True:
-        webgui_thread = threading.Thread(
-            target=lambda: create_http_mgmt_server().run(host=config['server']['bind'],
-                                                                      port=config['server']['webgui_port'],
-                                                                      use_reloader=False, threaded=True),
+        hdhr_webgui_thread = threading.Thread(
+            target=lambda: hdhr_create_http_mgmt_server().run(host=config['server']['bind'],
+                                                              port=config['server']['webgui_port'],
+                                                              use_reloader=False, threaded=True),
             daemon=True)
-        webgui_thread.start()
-        threads.append(webgui_thread)
+        hdhr_webgui_thread.start()
+        threads.append(hdhr_webgui_thread)
     if config['server']['enable_httpstream'] == True:
-        http_stream_thread = threading.Thread(
-            target=lambda: create_http_stream_server().run(host=config['server']['bind'], port=5004,
-                                                                        use_reloader=False, threaded=True),
+        hdhr_http_stream_thread = threading.Thread(
+            target=lambda: hdhr_create_http_stream_server().run(host=config['server']['bind'], port=5004,
+                                                                use_reloader=False, threaded=True),
             daemon=True)
-        http_stream_thread.start()
-        threads.append(http_stream_thread)
+        hdhr_http_stream_thread.start()
+        threads.append(hdhr_http_stream_thread)
     if config['server']['enable_broadcast'] == True:
-        broadcast_thread = threading.Thread(
-            target=lambda: CreateUDPBroadcastServer().run(ip='', port=65001),
+        hdhr_broadcast_thread = threading.Thread(
+            target=lambda: HDHRCreateUDPBroadcastServer().run(ip='', port=65001),
             daemon=True)
-        broadcast_thread.start()
-        threads.append(broadcast_thread)
+        hdhr_broadcast_thread.start()
+        threads.append(hdhr_broadcast_thread)
     if config['server']['enable_control'] == True:
-        control_thread = threading.Thread(
-            target=lambda: CreateTCPControlServer().run(ip=config['server']['bind'], port=65001),
+        hdhr_control_thread = threading.Thread(
+            target=lambda: HDHRCreateTCPControlServer().run(ip=config['server']['bind'], port=65001),
             daemon=True)
-        control_thread.start()
-        threads.append(control_thread)
-    #llmnr_thread = threading.Thread(
-    #    target=lambda: CreateLLMNRServer(config=config).run(ip='', port=5355),
+        hdhr_control_thread.start()
+        threads.append(hdhr_control_thread)
+    # hdhr_llmnr_thread = threading.Thread(
+    #    target=lambda: HDHRCreateLLMNRServer(config=config).run(ip='', port=5355),
     #    daemon=True)
-    #llmnr_thread.start() # not needed for now
-    #threads.append(llmnr_thread)
+    # hdhr_llmnr_thread.start() # not needed for now
+    # threads.append(hdhr_llmnr_thread)
 
     for thread in threads:
         thread.join()
